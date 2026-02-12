e576123b0462fc3ac4a13a6e083aa4e7dc566d5a
andrei <andrei.i.ivanov@gmail.com>
2026-02-12 10:07:39 +0900

    Static class to namespace

--- MeshtasticString.h
+++ MeshtasticString.h
@@ -12,35 +12,31 @@
 #include "MeshtasticTransport.h"
 #include "meshtastic/channel.pb.h"
 
-class MeshtasticString {
-public:
-    static std::string macAddress2string(uint64_t addr);
+namespace MeshtasticString {
+    std::string macAddress2string(uint64_t addr);
     // check is valid MAC address e.g. 9c:13:9e:a0:b7:5d
-    static bool isMacAddressString(const std::string &str);
-    static uint64_t MeshtasticString::string2macAddress(const std::string &str, bool *retValid = nullptr);
+    bool isMacAddressString(const std::string &str);
+    uint64_t string2macAddress(const std::string &str, bool *retValid = nullptr);
     // check is valid device file name e.g. COM4
-    static bool isDeviceFileNameString(const std::string &str);
-    static std::string transportType2String(MeshtasticTransportType type);
-    static std::string advertisementDataType2String(uint8_t dataType);
-    static std::string hex(const std::string &str);
-    static std::string hex(void *buffer, size_t size);
-    static std::string meshtasticMessageType2String(MeshtasticMessageType typ);
-    static std::string channelRole2String(meshtastic::Channel_Role role);
-    static std::string psk2String(const std::string &psk);
-    static std::string routingError2string(meshtastic::Routing_Error error);
-    static std::string port2string(meshtastic::PortNum portNum);
+    bool isDeviceFileNameString(const std::string &str);
+    std::string transportType2String(MeshtasticTransportType type);
+    std::string advertisementDataType2String(uint8_t dataType);
+    std::string hex(const std::string &str);
+    std::string hex(void *buffer, size_t size);
+    std::string meshtasticMessageType2String(MeshtasticMessageType typ);
+    std::string channelRole2String(meshtastic::Channel_Role role);
+    std::string psk2String(const std::string &psk);
+    std::string routingError2string(meshtastic::Routing_Error error);
+    std::string port2string(meshtastic::PortNum portNum);
 
-#ifdef _MSC_VER
-    static std::string hstring2string(const winrt::hstring &value);
-    static std::string characteristic2String(const winrt::Windows::Devices::Bluetooth::GenericAttributeProfile::GattCharacteristic &characteristic);
-    static std::string characteristicProperties2String(const winrt::Windows::Devices::Bluetooth::GenericAttributeProfile::GattCharacteristic &characteristic);
-    static std::string devicePairingStatus2string(winrt::Windows::Devices::Enumeration::DevicePairingResultStatus status);
-    static std::string gattCommunicationStatus2string(const winrt::Windows::Devices::Bluetooth::GenericAttributeProfile::GattCommunicationStatus &status);
-    static std::string sessionStatus2string(const winrt::Windows::Devices::Bluetooth::GenericAttributeProfile::GattSessionStatus status);
-    static std::string asyncStatus2string(const winrt::Windows::Foundation::AsyncStatus status);
-    static std::string currentTimeStamp();
-#endif
-
+    std::string hstring2string(const winrt::hstring &value);
+    std::string characteristic2String(const winrt::Windows::Devices::Bluetooth::GenericAttributeProfile::GattCharacteristic &characteristic);
+    std::string characteristicProperties2String(const winrt::Windows::Devices::Bluetooth::GenericAttributeProfile::GattCharacteristic &characteristic);
+    std::string devicePairingStatus2string(winrt::Windows::Devices::Enumeration::DevicePairingResultStatus status);
+    std::string gattCommunicationStatus2string(const winrt::Windows::Devices::Bluetooth::GenericAttributeProfile::GattCommunicationStatus &status);
+    std::string sessionStatus2string(const winrt::Windows::Devices::Bluetooth::GenericAttributeProfile::GattSessionStatus status);
+    std::string asyncStatus2string(const winrt::Windows::Foundation::AsyncStatus status);
+    std::string currentTimeStamp();
 };
 
 #endif
